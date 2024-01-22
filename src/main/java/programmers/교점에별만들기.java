beb476a46bc3934705bfc26fcf9d9525aaecafdf
SW-H <hsw08@naver.com>
2024-01-22 21:41:02 +0900

    binary search

--- src/main/java/programmers/교점에별만들기.java
+++ src/main/java/programmers/교점에별만들기.java
@@ -1,110 +1,110 @@
-package programmers;
-
-/*
- *  https://school.programmers.co.kr/learn/courses/30/lessons/87377
- *
- * */
-
-
-import java.util.*;
-import java.util.stream.*;
-import static java.util.Collections.*;
-
-public class 교점에별만들기 {
-	public static void main(String[] args) {
-		int[][] input =
-				{{1, -1, 0}, {2, -1, 0}, {4, -1, 0}};
-		// {{0, 1, -1}, {1, 0, -1}, {1, 0, 1}};
-		// {{2, -1, 4},
-		// 		{-2, -1, 4},
-		// 		{0, -1, 1},
-		// 		{5, -8, -12},
-		// 		{5, 8, 12}};
-		new Solution().solution(input);
-	}
-
-	static class Solution {
-		class Point {
-			public long r;
-			public long c;
-
-			public Point(long r, long c) {
-				this.r = r;
-				this.c = c;
-			}
-		}
-		List<Point> meetPoints = new ArrayList<>();
-		int[][] line;
-
-
-		public String[] solution(int[][] _line) {
-			line = _line;
-			addMeetPoints();
-
-			adjustPoints(meetPoints);
-
-			int height = (int)(max(meetPoints.stream().map(p -> p.r).collect(Collectors.toList())) + 1);
-			int width = (int)(max(meetPoints.stream().map(p -> p.c).collect(Collectors.toList())) + 1);
-			return draw(height, width, meetPoints);
-		}
-
-		private void addMeetPoints() {
-
-			for (int i = 0; i < line.length; i++) {
-				for (int j = i + 1; j < line.length; j++) {
-					long A = line[i][0];
-					long B = line[i][1];
-					long E = line[i][2];
-
-					long C = line[j][0];
-					long D = line[j][1];
-					long F = line[j][2];
-
-					if ((A * D - B * C == 0) ||
-							((E * C - A * F) % (A * D - B * C) != 0) ||
-							((B * F - E * D) % (A * D - B * C) != 0)) {
-						continue;
-					}
-					long meetR = (E * C - A * F) / (A * D - B * C); // y
-					long meetC = (B * F - E * D) / (A * D - B * C); // x
-
-					meetPoints.add(new Point(meetR, meetC));
-
-				}
-			}
-		}
-
-		private String[] draw(int height, int width, List<Point> meetPoints) {
-			String oneLine = ".".repeat(width);
-			StringBuilder[] result = new StringBuilder[height];
-			for (int i = 0; i < height; i++) {
-				result[i] = new StringBuilder(oneLine);
-			}
-
-			for (Point meetpoint : meetPoints) {
-				result[(int)meetpoint.r].setCharAt((int)meetpoint.c, '*');
-			}
-
-			String[] ret = new String[height];
-
-			for (int i = result.length - 1; i >= 0; i--) {
-				ret[i] = result[height - i - 1].toString();
-			}
-			return ret;
-		}
-
-		private void adjustPoints(List<Point> meetPoints) {
-			int rAdjustmentValue = (int)meetPoints.get(0).r;
-			int cAdjustmentValue = (int)meetPoints.get(0).c;
-
-			for (Point point : meetPoints) {
-				rAdjustmentValue = (int)Math.min(rAdjustmentValue, point.r);
-				cAdjustmentValue = (int)Math.min(cAdjustmentValue, point.c);
-			}
-			for (Point point : meetPoints) {
-				point.r -= rAdjustmentValue;
-				point.c -= cAdjustmentValue;
-			}
-		}
-	}
-}
+// package programmers;
+//
+// /*
+//  *  https://school.programmers.co.kr/learn/courses/30/lessons/87377
+//  *
+//  * */
+//
+//
+// import java.util.*;
+// import java.util.stream.*;
+// import static java.util.Collections.*;
+//
+// public class 교점에별만들기 {
+// 	public static void main(String[] args) {
+// 		int[][] input =
+// 				{{1, -1, 0}, {2, -1, 0}, {4, -1, 0}};
+// 		// {{0, 1, -1}, {1, 0, -1}, {1, 0, 1}};
+// 		// {{2, -1, 4},
+// 		// 		{-2, -1, 4},
+// 		// 		{0, -1, 1},
+// 		// 		{5, -8, -12},
+// 		// 		{5, 8, 12}};
+// 		new Solution().solution(input);
+// 	}
+//
+// 	static class Solution {
+// 		class Point {
+// 			public long r;
+// 			public long c;
+//
+// 			public Point(long r, long c) {
+// 				this.r = r;
+// 				this.c = c;
+// 			}
+// 		}
+// 		List<Point> meetPoints = new ArrayList<>();
+// 		int[][] line;
+//
+//
+// 		public String[] solution(int[][] _line) {
+// 			line = _line;
+// 			addMeetPoints();
+//
+// 			adjustPoints(meetPoints);
+//
+// 			int height = (int)(max(meetPoints.stream().map(p -> p.r).collect(Collectors.toList())) + 1);
+// 			int width = (int)(max(meetPoints.stream().map(p -> p.c).collect(Collectors.toList())) + 1);
+// 			return draw(height, width, meetPoints);
+// 		}
+//
+// 		private void addMeetPoints() {
+//
+// 			for (int i = 0; i < line.length; i++) {
+// 				for (int j = i + 1; j < line.length; j++) {
+// 					long A = line[i][0];
+// 					long B = line[i][1];
+// 					long E = line[i][2];
+//
+// 					long C = line[j][0];
+// 					long D = line[j][1];
+// 					long F = line[j][2];
+//
+// 					if ((A * D - B * C == 0) ||
+// 							((E * C - A * F) % (A * D - B * C) != 0) ||
+// 							((B * F - E * D) % (A * D - B * C) != 0)) {
+// 						continue;
+// 					}
+// 					long meetR = (E * C - A * F) / (A * D - B * C); // y
+// 					long meetC = (B * F - E * D) / (A * D - B * C); // x
+//
+// 					meetPoints.add(new Point(meetR, meetC));
+//
+// 				}
+// 			}
+// 		}
+//
+// 		private String[] draw(int height, int width, List<Point> meetPoints) {
+// 			String oneLine = ".".repeat(width);
+// 			StringBuilder[] result = new StringBuilder[height];
+// 			for (int i = 0; i < height; i++) {
+// 				result[i] = new StringBuilder(oneLine);
+// 			}
+//
+// 			for (Point meetpoint : meetPoints) {
+// 				result[(int)meetpoint.r].setCharAt((int)meetpoint.c, '*');
+// 			}
+//
+// 			String[] ret = new String[height];
+//
+// 			for (int i = result.length - 1; i >= 0; i--) {
+// 				ret[i] = result[height - i - 1].toString();
+// 			}
+// 			return ret;
+// 		}
+//
+// 		private void adjustPoints(List<Point> meetPoints) {
+// 			int rAdjustmentValue = (int)meetPoints.get(0).r;
+// 			int cAdjustmentValue = (int)meetPoints.get(0).c;
+//
+// 			for (Point point : meetPoints) {
+// 				rAdjustmentValue = (int)Math.min(rAdjustmentValue, point.r);
+// 				cAdjustmentValue = (int)Math.min(cAdjustmentValue, point.c);
+// 			}
+// 			for (Point point : meetPoints) {
+// 				point.r -= rAdjustmentValue;
+// 				point.c -= cAdjustmentValue;
+// 			}
+// 		}
+// 	}
+// }
--- src/main/java/programmers/징검다리.java
+++ src/main/java/programmers/징검다리.java
@@ -1,2 +1,117 @@
-package programmers;public class 징검다리 {
+package programmers;
+
+import java.util.Arrays;
+
+public class 징검다리 {
+	public static void main(String[] args) {
+		int solution = new Solution().solution(25, new int[] {2, 14, 11, 21, 17}, 2);
+
+		System.out.println(solution);
+	}
+
+	static class Solution {
+
+		final int MIN_DISTANCE = 1;
+		final int MAX_DISTANCE = 1_000_000_000;
+
+		public int solution(int distance, int[] rocks, int n) {
+			rocks = Arrays.copyOf(rocks, rocks.length + 1);
+			rocks[rocks.length - 1] = distance;
+			int answer = solve(MIN_DISTANCE, MAX_DISTANCE, Arrays.stream(rocks).sorted().toArray(), n);
+
+			return answer;
+		}
+
+		private int solve(int start, int end, int[] rocks, int n) {
+			while (end - start > 1) {
+				int min = (start + end) / 2;
+				if (isValid(min, rocks, n)) {
+					start = min;
+				} else {
+					end = min;
+				}
+			}
+			return start;
+		}
+
+		private boolean isValid(int minDistance, int[] rocks, int n) {
+			int removed = 0;
+			int last = 0;
+			for (int rock : rocks) {
+				if (rock - last < minDistance) {
+					removed++;
+					continue;
+				}
+				last = rock;
+			}
+
+			return removed <= n;
+		}
+
+	}
+
+	// 시도 1. 시간 초과 및 틀린 풀이
+	/*
+	static class Solution {
+		List<Integer> minDistance = new ArrayList<>();
+
+		public int solution(int distance, int[] rocks, int n) {
+			int answer = 0;
+			List<Integer> sortedRocks = Arrays.stream(rocks).sorted().boxed().collect(Collectors.toList());
+			sortedRocks.add(distance);
+
+			getMinDistances(new ArrayList<>(), sortedRocks, n, distance);
+
+			for (Integer dist : minDistance) {
+				if (answer < dist) {
+					answer = dist;
+				}
+			}
+
+			return answer;
+		}
+
+		private void getMinDistances(
+			List<Integer> currentlySelected,
+			List<Integer> toSelect,
+			int n,
+			int distance
+		) {
+			if (n < 0) {
+
+				currentlySelected.add(distance);
+				minDistance.add(calculateMinDistance(currentlySelected));
+
+			}
+
+			for (int i = 0; i < toSelect.size(); i++) {
+				currentlySelected.add(toSelect.get(i));
+				getMinDistances(
+					new ArrayList<>(currentlySelected),
+					new ArrayList<>(toSelect.subList(i + 1, toSelect.size())),
+					n - 1,
+					distance
+				);
+				currentlySelected.remove(currentlySelected.size() - 1);
+				// if (!currentlySelected.isEmpty()) {
+				// }
+			}
+
+		}
+
+		private int calculateMinDistance(List<Integer> rocks) {
+			int ret = 1_000_000_000;
+
+			rocks.sort(Comparator.naturalOrder());
+
+			for (int i = 1; i < rocks.size(); i++) {
+				if (rocks.get(i) - rocks.get(i - 1) < ret) {
+					ret = rocks.get(i) - rocks.get(i - 1);
+				}
+			}
+
+			return ret;
+		}
+	}
+	*/
 }
