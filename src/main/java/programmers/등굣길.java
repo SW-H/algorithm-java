3a2aaa4809a84f45a8e40b35d8e90c158969c09e
SW-H <hsw08@naver.com>
2024-03-20 09:05:32 +0900

    solved : DP

--- src/main/java/programmers/등굣길.java
+++ src/main/java/programmers/등굣길.java
@@ -1,48 +1,69 @@
 package programmers;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 public class 등굣길 {
-
 	class Solution {
 
-		int[] dr = {0, 1}; //우,하
-		int[] dc = {1, 0}; //우,하
-		int RPOS = 0;
-		int CPOS = 0;
+		int MOD = 1_000_000_007;
+
+		int dr[] = {0, -1}; //좌, 상
+		int dc[] = {-1, 0}; //좌, 상
+
+		boolean[][] isPuddle;
+		int[][] mem;
 
 		public int solution(int m, int n, int[][] puddles) {
-			// +) 최소길이 파악?
-			return dfs(m, n, puddles);
+
+			initialize(m, n, puddles);
+
+			return answer(n - 1, m - 1);
 		}
 
-		// width = m
-		// height = n
-
-		public int dfs(int m, int n, int[][] puddles) {
-			Queue<int[]> q = new LinkedList();
-			q.offer(new int[] {0, 0});
-
-			// 최종 : (n-1,m-1)
-
-			while (!q.isEmpty()) {
-				int[] cur = q.poll();
-
-				int r = cur[RPOS];
-				int c = cur[CPOS];
-
-				for (int d = 0; d < dr.length; d++) { // 우,하 인접
-					if ((0 > r + dr[d]) || (r + dr[d] >= n) || (0 > c + dc[d]) || (c + dc[d] >= m)) { // out of bound
-						continue;
-					}
-
-				}
-
+		public int answer(int r, int c) {
+			// answer(r,c) = answer(r,c-1) + answer(r-1,c);
+			if (((r < 0) || (c < 0)) || isPuddle[r][c]) {
+				return 0;
+			}
+			if ((r == 0) && (c == 0)) {
+				return 1;
+			}
+			if (mem[r][c] >= 0) {
+				return mem[r][c];
 			}
 
-			return 0;
+			int ret = 0;
+
+			for (int d = 0; d < 2; d++) {
+				ret = (ret + answer(r + dr[d], c + dc[d])) % MOD;
+			}
+
+			return (mem[r][c] = ret % MOD);
 		}
+
+		public void initialize(int m, int n, int[][] puddles) {
+			isPuddle = new boolean[n][m];
+			mem = new int[n][m];
+
+			for (int[] row : mem) {
+				Arrays.fill(row, -1);
+			}
+
+			for (boolean[] row : isPuddle) {
+				Arrays.fill(row, false);
+			}
+
+			if ((puddles.length <= 0) || (puddles[0].length <= 0)) {
+				return;
+			}
+
+			for (int i = 0; i < puddles.length; i++) {
+				int[] puddle = puddles[i];
+				isPuddle[puddle[1] - 1][puddle[0] - 1] = true;
+			}
+
+		}
+
 	}
 
 }
