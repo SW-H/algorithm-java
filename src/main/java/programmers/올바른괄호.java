ee14739b889548e4c188486b30c260a191f99e9d
SW-H <hsw08@naver.com>
2024-07-30 16:14:00 +0900

    solved : data structure

---
+++ src/main/java/programmers/올바른괄호.java
@@ -0,0 +1,32 @@
+package programmers;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+class 올바른괄호 {
+	public static void main(String[] args) {
+		//        String[] input = {}
+		System.out.print(new Solution().solution("(())()"));
+	}
+
+	static class Solution {
+		boolean solution(String s) {
+			int opened = 0;
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) == '(') {
+					opened++;
+					continue;
+				}
+				if (opened <= 0) {
+					return false;
+				}
+				opened--;
+
+			}
+
+			if (opened == 0) {
+				return true;
+			}
+			return false;
+		}
+	}
+}
